2e2dab6daadb5cb048267c5cb52eff446eab9033
Shayan Mahtabi <shayan.mhtb@gmail.com>
2025-07-05 20:40:20 +0200

    ShadCN Drawer component updated for typescript

--- web/components/ui/drawer/Drawer.vue
+++ web/components/ui/drawer/Drawer.vue
@@ -1,22 +1,23 @@
 <script lang="ts" setup>
-import type { DrawerRootEmits, DrawerRootProps } from 'vaul-vue'
-import { useForwardPropsEmits } from 'reka-ui'
-import { DrawerRoot } from 'vaul-vue'
+import { defineProps, defineEmits, computed } from 'vue';
+import { DrawerRoot } from 'vaul-vue';
+import { useForwardPropsEmits } from 'reka-ui';
+import type { DrawerRootEmits, DrawerRootProps } from 'vaul-vue';
 
 const props = withDefaults(defineProps<DrawerRootProps>(), {
   shouldScaleBackground: true,
-})
+});
 
-const emits = defineEmits<DrawerRootEmits>()
+const emits = defineEmits<DrawerRootEmits>();
 
-const forwarded = useForwardPropsEmits(props, emits)
+const forwarded = useForwardPropsEmits(props, emits);
+const forwardedObject = computed(() => {
+  return forwarded.value as Record<string, unknown>;
+});
 </script>
 
 <template>
-  <DrawerRoot
-    data-slot="drawer"
-    v-bind="forwarded"
-  >
+  <DrawerRoot data-slot="drawer" v-bind="forwardedObject">
     <slot />
   </DrawerRoot>
 </template>
